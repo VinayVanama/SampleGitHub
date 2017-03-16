39cca9841f2e9a384d453efb0b5d96a73c75bc0d
CYKUL App's <vinay@cykul.com>
2017-03-16 18:05:24 +0530

    This is a Sample Application pushed to Git

--- app/src/main/java/com/example/cykulapps/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cykulapps/myapplication/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.cykulapps.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = (TextView)findViewById(R.id.hello);
     }
 }
